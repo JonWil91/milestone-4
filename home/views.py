2b6d604ab8b5315cb8a2f24ac49380a7db1c7e02
Jon Williams <jrwilliams100@hotmail.co.uk>
2020-07-06 14:34:06 +0000

    Tested first url/view file functionality

--- home/views.py
+++ home/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+
+def index(request):
+    """ A view to return the index page """
+    return HttpResponse('My index url is working')
