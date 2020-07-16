9cf3538e6735349df5683b299ffa7aa0c612f8fa
Jon Williams <jrwilliams100@hotmail.co.uk>
2020-07-16 12:18:57 +0000

    Amended products models and amended template and views

--- products/views.py
+++ products/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
+from .models import Product
 
 # Create your views here.
 
 
-def products(request):
-    """ A view to return the gallery page """
-    return render(request, 'products/products.html')
+def all_products(request):
+    """ A view to show all products, including sorting and search queries """
+
+    products = Product.objects.all()
+
+    context = {
+        'products': products,
+    }
+
+    return render(request, 'products/products.html', context)
