1c09658f64dab32b14d77dec116af13af138d0df
Jon Williams <jrwilliams100@hotmail.co.uk>
2020-08-10 11:56:25 +0000

    Added contact model, form and views

--- contact/views.py
+++ contact/views.py
@@ -1,8 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from .forms import ContactForm
 
 # Create your views here.
 
 
 def contact(request):
-    """ A view to return the contact page """
-    return render(request, 'contact/contact.html')
+    """ Use the contact form """
+    if request.method == 'POST':
+        form = ContactForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+        else:
+            messages.error(request, 'Failed to add product. Please ensure the form is valid.')
+    else:
+        form = ContactForm()
+        
+    template = 'contact/contact.html'
+    context = {
+        'form': form,
+    }
+
+    return render(request, template, context)
