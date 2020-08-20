11f866b0989b909c8c70dd08944ad0d77e8fe5aa
Jon Williams <jrwilliams100@hotmail.co.uk>
2020-08-20 17:02:26 +0000

    Added responsive design to layout

---
+++ products/static/products/js/frame_colour.js
@@ -0,0 +1,17 @@
+let div = document.getElementById("blueDiv");
+
+    function blue() {
+        div.setAttribute("id", "blueDiv");
+    }
+
+    function yellow() {
+        div.setAttribute("id", "yellowDiv");
+    }
+
+    function red() {
+        div.setAttribute("id", "redDiv");
+    }
+
+    function green() {
+        div.setAttribute("id", "greenDiv");
+    }
