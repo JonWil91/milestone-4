4dce94a821542ac62471aa0fc1a7ac0711c387cf
Jon Williams <jrwilliams100@hotmail.co.uk>
2020-08-21 14:20:54 +0000

    Removed secret key and set debug

--- milestone4/settings.py
+++ milestone4/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'eumcyew2r#!nmv459p=_(t2cmyq2gf_lew0f(%a3!h$+nym=s-'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['jw-photography.herokuapp.com', 'localhost']
 
